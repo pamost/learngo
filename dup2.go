d6daa3ee775777569abea57b842c81aaf288ce22
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2018-05-23 11:42:11 +0600

    update dup2

--- dup2.go
+++ dup2.go
@@ -12,22 +12,19 @@ import (
 )
 
 func main() {
+
 	counts := make(map[string]int)
 
-	files := os.Args[1:]
-	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
-		}
+	file := "file.txt"
+
+	f, err := os.Open(file)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 	}
+
+	countLines(f, counts)
+	f.Close()
+
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
@@ -38,7 +35,9 @@ func main() {
 func countLines(f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		if input.Text() == "" { break }
+		if input.Text() == "" {
+			break
+		}
 		counts[input.Text()]++
 	}
 }
