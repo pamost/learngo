85f8cc8119a226bc5b6a85488181c6a797008e16
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2018-05-25 17:02:31 +0600

    update dup3

--- dup3.go
+++ dup3.go
@@ -13,16 +13,19 @@ import (
 
 func main() {
 	counts := make(map[string]int)
-	for _, filename := range os.Args[1:] {
-		data, err := ioutil.ReadFile(filename)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
-			continue
-		}
-		for _, line := range strings.Split(string(data), "\n") {
-			counts[line]++
-		}
+
+	fileText := "file.txt"
+
+	data, err := ioutil.ReadFile(fileText)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "dup3: %v\n", err)
 	}
+
+	for _, line := range strings.Split(string(data), "\n") {
+		counts[line]++
+	}
+
 	for line, n := range counts {
 		if n > 1 {
 			fmt.Printf("%d\t%s\n", n, line)
