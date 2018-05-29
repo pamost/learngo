02b7376477c4bb306f87f1f0b44b0957f8c865d1
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2018-05-29 16:02:28 +0600

    fetch7ex

--- fetch7ex.go
+++ fetch7ex.go
@@ -3,26 +3,25 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 )
 
 func main() {
-	u := []string{"http://golang.org", "http://megaweb.kz/"}
+	u := []string{"http://golang.org", "http://megaweb.kz"}
 	for _, url := range u[0:] {
+
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 		}
-		fmt.Println(resp)
 
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
+		if _, err := io.Copy(os.Stdout, resp.Body); err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: чтение %s: %v\n", url, err)
 			os.Exit(1)
 		}
-		fmt.Printf("%s", b)
+
+		resp.Body.Close()
 	}
 }
