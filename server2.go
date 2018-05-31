4cb719128381769dd145ed379ab3eafdd9bc853c
Pavel Mostovoy <p.a.mostovoy@gmail.com>
2018-05-31 17:16:44 +0600

    server2

---
+++ server2.go
@@ -0,0 +1,32 @@
+// Server1 - минимальный "echo"-сервер со счетчиком запросов
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", base) // Каждый запрос обрабатывает обработчик
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+// Обработчик возвращает компонент пути из URL запроса
+func base(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count = %d\n", count)
+	mu.Unlock()
+}
